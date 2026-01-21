f7fd0a6e3df565c735b310b8f6e9fd7f938c92a5
Matyas Pydych <matyas.pydych@gmail.com>
2026-01-21 22:22:28 +0100

    exeption experiment 1

--- app.py
+++ app.py
@@ -3,22 +3,26 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 def rozklad(n):
-    faktory = []
-    d = 2
-    while d * d <= n:
-        while n % d == 0:
-            faktory.append(d)
-            n //= d
-        d += 1
-    if n > 1:
-        faktory.append(n)
-    return faktory
+    #try:
+        faktory = []
+        d = 2
+        while d * d <= n:
+            while n % d == 0:
+                faktory.append(d)
+                n //= d
+            d += 1
+        if n > 1:
+            faktory.append(n)
+        return faktory
+    #except Exception:
+        #print("Something went wrong")
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     vysledek = None
     if request.method == "POST":
-        n = int(request.form["number"])
+        n = int(request.form["string"])
         vysledek = rozklad(n)
     return render_template("index.html", vysledek=vysledek)
 
